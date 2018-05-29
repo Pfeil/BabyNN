aea93f4fa36074b1a1705ae9f4c4ca251dedba3a
Andreas Pfeil <andreas.pfeil@student.kit.edu>
2018-05-29 20:17:17 +0200

    Initial Commit. MNIST example is still not working.

---
+++ examples/mnist1.rs
@@ -0,0 +1,108 @@
+extern crate csv;
+extern crate simple_nn;
+
+use csv::ReaderBuilder;
+use simple_nn::{Sample, Vector, NN};
+use std::f64;
+
+/// This struct is supposed to represent the given data, not adjusted to the neural net.
+/// Due to the implemented From-Trait, it is easily to create a Sample from it.
+#[derive(Debug)]
+struct LabeledImage {
+    label: u8,
+    image: Vec<u8>,
+}
+
+impl From<LabeledImage> for Sample {
+    fn from(img: LabeledImage) -> Self {
+        // Note: The return value of the sigmoid function (and therefore the output)
+        // will not reach 1.0 or 0.0, but only values inbetween.
+        // So the ideal label [0,0,0,1,0,0,0,0,0,0] will be
+        // [.01,.01,.01,.99,.01,.01,.01,.01,.01,.01] to work around this.
+        let mut label = vec![0.01; 10]; // most values are (nearly) zero, so we begin like this.
+        label[img.label as usize] = 0.99; // set the label.
+        let label = label; // remove mutability just because we do not need it anymore.
+        let image: Vec<f64> = img.image
+            .into_iter()
+            .map(|byte| byte_into_nn_compatible(byte))
+            .collect();
+        Sample {
+            input: Vector::new(image),
+            target: Vector::new(label),
+        }
+    }
+}
+
+/// Since our neural network uses the sigmoid function, we need to adjust our byte data to fit into the interval [0,1]. More percisely, it needs to exclude 0.0 and 1.1 specifically, since the sigmoid function used will never return 0 or 1. This means our outputs should be in this interval, and a general rule says that the input should be based on the same interval as the output.
+fn byte_into_nn_compatible(num: u8) -> f64 {
+    ((num as f64 / 255.0) * 0.99) + 0.01
+}
+
+fn read_labeled_images(path: &str) -> Vec<LabeledImage> {
+    //let path = "examples/mnist_train_100.csv";
+    let mut rdr = ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(path)
+        .expect(&format!("File {} not found.", path));
+    let mut samples: Vec<LabeledImage> = Vec::new();
+    for result in rdr.records() {
+        let record = result.expect("Result unwrap did not work.");
+        let stuff: Vec<u8> = record // record contains csv strings (bytes as strings) values
+            .into_iter()
+            .map(|s| s.parse().expect("could not parse into u8")) // str --> u8 (byte)
+            .collect(); // u8 iterator --> Vec<u8>
+        let (head, tail) = stuff
+            .split_first()
+            .expect("fail: tried to split an empty list.");
+        samples.push(LabeledImage {
+            label: head.clone(),
+            image: tail.to_vec(),
+        });
+    }
+    //println!("{:?}", samples);
+    samples
+}
+
+fn main() {
+    // load training data
+    let training_samples: Vec<Sample> = read_labeled_images("examples/mnist_train_100.csv")
+        .into_iter() // for each sample... (iterating over them)
+        .map(|img| img.into()) // into() here equals to Sample::from(img). Rust is magic :)
+        .collect(); // collect all samples to match the given type of `training_samples`.
+    // set up network topology and learning rate
+    // Note: I assume here (inp = 784 and out = 10),
+    // that all our input data (the csv files) has this given size,
+    // and our output vector has always length 10. I did not validate this to shorten the code.
+    let inp = 784;
+    let hid = 100;
+    let out = 10;
+    let learn_rate = 0.3;
+    let mut nn = NN::new(inp, hid, out, learn_rate);
+    // train the network
+    for _epoch in 0..10 {
+        for sample in &training_samples {
+            println!("training sample...");
+            nn.train(&sample);
+        }
+    }
+    // test the network
+    let test_samples: Vec<Sample> = read_labeled_images("examples/mnist_test_10.csv")
+        .into_iter()
+        .map(|img| img.into())
+        .collect();
+    let mut scorecard: (usize, usize) = (0,0);
+    for sample in test_samples {
+        let output = nn.query(&sample.input);
+        // println!("{}", &sample.target);
+        let result = &output.argmax();
+        let target = &sample.target.argmax().0;
+        println!("{} ({}) -- {}", result.0, target, result.1);
+        if result.0 == *target {
+            scorecard.0 += 1;
+        } else {
+            scorecard.1 += 1;
+        }
+    }
+    let precision = scorecard.0 as f64 / (scorecard.0 as f64 + scorecard.1 as f64);
+    println!("precision: {}", precision);
+}
---
+++ src/lib.rs
@@ -0,0 +1,163 @@
+//! This is a crate for educational purposes.
+//! It's inspired by the book "Make your own Neural Network" by Tariq Rahshid
+//! (actually, it was the german version of the book, "Neuronale Netze selbst programmieren",
+//! published by O'reilly).
+
+extern crate rand;
+extern crate rulinalg;
+
+mod activation_fn;
+
+use rand::distributions::{Distribution, Normal};
+pub use rulinalg::matrix::BaseMatrix;
+use rulinalg::matrix::Matrix;
+pub use rulinalg::vector::Vector;
+
+use activation_fn::sigmoid;
+
+/// This represents a minimal neural net.
+/// # Examples
+/// ```
+/// let input_len = 2;
+/// let hidden_len = 3;
+/// let output_len = 5;
+/// let learn_rate = 0.3;
+/// let nn = simple_nn::NN::new(input_len, hidden_len, output_len, learn_rate);
+/// // TODO Train your net before querying, otherwise it will output something more or less random.
+/// let input = simple_nn::Vector::new(vec![5., 0.]);
+/// let output = nn.query(&input);
+/// assert_eq!(output.size(), output_len);
+/// ```
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct NN {
+    learn_rate: f64,
+    weights_ih: Matrix<f64>,
+    weights_ho: Matrix<f64>,
+}
+
+#[allow(dead_code)]
+impl NN {
+    /// construct a simpe 1-Layer feed forward net, using the given
+    /// topology (the number of neurons for each layer).
+    pub fn new(num_input: usize, num_hidden: usize, num_output: usize, learn_rate: f64) -> Self {
+        // prepare random number generator.
+        let sqrt_num_input = (num_input as f64).powf(-0.5);
+        let sqrt_num_hidden = (num_input as f64).powf(-0.5);
+        let random_in = Normal::new(0.0, sqrt_num_input);
+        let random_hid = Normal::new(0.0, sqrt_num_hidden);
+        let rng = &mut rand::thread_rng();
+        // prepare vectors with random numbers in a range that is supposed to be not that bad.
+        let val_ih: Vec<f64> = (0..num_hidden * num_input)
+            .map(|_| random_in.sample(rng))
+            .collect();
+        let val_ho: Vec<f64> = (0..num_output * num_hidden)
+            .map(|_| random_hid.sample(rng))
+            .collect();
+        // create Matrices using the random numbers from the vectors
+        let weights_ih = Matrix::new(num_hidden, num_input, val_ih);
+        let weights_ho = Matrix::new(num_output, num_hidden, val_ho);
+        NN {
+            learn_rate,
+            weights_ih,
+            weights_ho,
+        }
+    }
+
+    pub fn train(&mut self, sample: &Sample) {
+        assert_eq!(self.num_input_neurons(), sample.input.size());
+        assert_eq!(self.num_output_neurons(), sample.target.size());
+        let (hidden_out, output) = self.query_all(&sample.input);
+        // Backpropagation
+        let output_error = &sample.target - &output;
+        let hidden_error = self.weights_ho.transpose() * &output_error;
+        // Gradient Descent
+        let ones = Vector::from_fn(self.num_output_neurons(), |_| 1f64);
+        let ones_minus_output = &ones - &output;
+        self.weights_ho += self.learn_rate
+            * (output_error
+                .elemul(&output.clone().elemul(&ones_minus_output))
+                .dot(&hidden_out));
+        let ones = Vector::from_fn(self.num_hidden_neurons(), |_| 1f64);
+        let ones_minus_hidden_out = ones - &hidden_out;
+        self.weights_ih += self.learn_rate
+            * (hidden_error
+                .elemul(&hidden_out.clone().elemul(&ones_minus_hidden_out))
+                .dot(&sample.input));
+    }
+
+    /// Process the net to get an output vector to the given sample.
+    /// It will only return the output vector. To get the vector of the hidden layer too,
+    /// see `query_all`.
+    pub fn query(&self, input: &Vector<f64>) -> Vector<f64> {
+        let (_, output) = self.query_all(input);
+        output
+    }
+
+    /// A helper function that does the query of the net,
+    /// but also returns the output of the hidden layer.
+    /// It is used in the training routine only and is therefore private.
+    fn query_all(&self, input: &Vector<f64>) -> (Vector<f64>, Vector<f64>) {
+        let hidden_summed: Vector<f64> = &self.weights_ih * input;
+        assert_eq!(&hidden_summed.size(), &self.weights_ih.rows());
+        let hidden_activated: Vector<f64> = hidden_summed.into_iter().map(|x| sigmoid(x)).collect();
+        assert_eq!(&hidden_activated.size(), &self.weights_ih.rows());
+        let output_summed: Vector<f64> = &self.weights_ho * &hidden_activated;
+        assert_eq!(&output_summed.size(), &self.weights_ho.rows());
+        let output_activated: Vector<f64> = output_summed.into_iter().map(|x| sigmoid(x)).collect();
+        assert_eq!(&output_activated.size(), &self.weights_ho.rows());
+        (hidden_activated, output_activated)
+    }
+
+    fn num_input_neurons(&self) -> usize {
+        self.weights_ih.cols()
+    }
+
+    fn num_hidden_neurons(&self) -> usize {
+        self.weights_ih.rows()
+    }
+
+    fn num_output_neurons(&self) -> usize {
+        self.weights_ho.rows()
+    }
+}
+
+/// `Sample` Represents an input vector with the desired output vector.
+/// This way, a Neural Net can learn what to return for a given data.
+#[derive(Debug)]
+pub struct Sample {
+    pub input: Vector<f64>,
+    pub target: Vector<f64>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn net_can_tell_amount_of_neurons() {
+        // TODO do this for random generated amounts or so
+        let inp = 3;
+        let hid = 2;
+        let out = 1;
+        let learn_rate = 0.3;
+        let nn = NN::new(inp, hid, out, learn_rate);
+        assert_eq!(inp, nn.num_input_neurons());
+        assert_eq!(hid, nn.num_hidden_neurons());
+        assert_eq!(out, nn.num_output_neurons());
+    }
+
+    #[test]
+    fn create_and_query_simple_sanity_check() {
+        // TODO do this for random generated amounts or so
+        let inp = 3;
+        let hid = 2;
+        let out = 1;
+        let learn_rate = 0.3;
+        let nn = NN::new(inp, hid, out, learn_rate);
+        let input = Vector::new(vec![5.; inp]);
+        let output = nn.query(&input);
+        assert_eq!(out, output.size());
+        println!("{}", output);
+    }
+}
